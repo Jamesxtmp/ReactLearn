2ca948a242c16393c8d02342de00e7aea6055cb9
Jamasxtmp <gen22xtmp@gmail.com>
2022-04-01 13:07:37 -0600

    Added Calculadora

---
+++ src/Seccion13_Calculadora-de-Interes/App.js
@@ -0,0 +1,8 @@
+const App = () => {
+    return(
+        <div>
+            Calculadora
+        </div>
+    )
+}
+export default App
--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import ReactDOM  from "react-dom"
-<<<<<<< HEAD
 import App from './Seccion13_Calculadora-de-Interes/App'
-=======
-import App from './Seccion11_Buscador-de-Imagenes/App'
->>>>>>> 867d00f978be0db3c39058dccc2e2594f7238b84
 import './main.css'
 
 ReactDOM.render(
